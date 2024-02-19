1498d7157f547132c757a3f0f1ffaa551ed2f614
Nathan Guiard <nguiard@student.42.fr>
2024-02-19 05:34:35 -0500

    Seems to be working

--- P3/scripts/_nguiard-3.sh
+++ P3/scripts/_nguiard-3.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
 # Setting up the bridge
-# /!\ POUR L'INSTANT COMMENTEES POUR SE FIER AU SUJET/VIDEO /!\
-# ip link add br0 type bridge
-# ip link set dev br0 up
-# ip link add vxlan10 type vxlan id 10 dstport 4789
-# ip link set dev vxlan10 up
-# brctl addif br0 vxlan10
-# brctl addif br0 eth0
+ip link add br0 type bridge
+ip link set dev br0 up
+ip link add vxlan10 type vxlan id 10 dstport 4789
+ip link set dev vxlan10 up
+brctl addif br0 vxlan10
+brctl addif br0 eth0
 
 # "Switch" configuration
 vtysh << EOF
--- P3/setup.sh
+++ P3/setup.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+if [ $(docker ps -q | wc -l) -gt 4 ]
+then
+	echo 'More than 4 docker container running, Aborting'
+	exit 1
+fi
+
 for id in $(docker ps -q); do
 	hostname=$(docker inspect -f '{{.Config.Hostname}}' $id)
 	script=scripts/$(echo $hostname).sh
 	echo "applying $script config to $hostname ($id)"
 	cat $script | docker exec -i $id sh
-	sleep 1
 done
